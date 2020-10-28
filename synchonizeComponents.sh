f8a084760c4a33aed56bddf1002ce619fa8f50aa
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-10-28 16:26:34 +0100

    Fix new component names.

--- synchonizeComponents.sh
+++ synchonizeComponents.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR >/dev/null
 declare -a SrcArray=("src/main/java/eu/internetofus/common" "src/main/resources/eu/internetofus/common" "src/test/java/eu/internetofus/common" "src/test/resources/eu/internetofus/common")
-declare -a ComponentArray=("wenet-interaction-protocol-engine" "wenet-profile-manager" "wenet-task-manager")
+declare -a ComponentArray=("profile-manager" "task-manager" "interaction-protocol-engine")
 for src in "${SrcArray[@]}"; do
     echo "--- $src  ---"
     for component in "${ComponentArray[@]}"; do
