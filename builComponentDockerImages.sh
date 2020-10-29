c42598b13ea673f947f3b2c31370ed99406a7083
Bruno Rosell i Gui (UDT-IA, IIIA-CSIC) <rosell@localhost>
2020-10-29 09:10:15 +0100

    Fix component names without wenet-

--- builComponentDockerImages.sh
+++ builComponentDockerImages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR >/dev/null
-declare -a ComponentArray=("wenet-interaction-protocol-engine" "wenet-profile-manager" "wenet-task-manager")
+declare -a ComponentArray=("profile-manager" "task-manager" "interaction-protocol-engine")
 for component in "${ComponentArray[@]}"; do
   echo "* $component"
   pushd $DIR/../$component > /dev/null
