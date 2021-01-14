b07c7f71b50062efe35645fa24097b31857fc531
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-01-14 17:49:59 +0100

    Prepare for release.

--- src/test/java/eu/internetofus/common/Containers.java
+++ src/test/java/eu/internetofus/common/Containers.java
@@ -92,12 +92,12 @@ public class Containers {
   /**
    * The name of the WeNet task manager docker container to use.
    */
-  public static final String WENET_TASK_MANAGER_DOCKER_NAME = "internetofus/task-manager:0.6.0";
+  public static final String WENET_TASK_MANAGER_DOCKER_NAME = "internetofus/task-manager:0.7.0";
 
   /**
    * The name of the WeNet interaction manager docker container to use.
    */
-  public static final String WENET_INTERACTION_PROTOCOL_ENGINE_DOCKER_NAME = "internetofus/interaction-protocol-engine:0.12.0";
+  public static final String WENET_INTERACTION_PROTOCOL_ENGINE_DOCKER_NAME = "internetofus/interaction-protocol-engine:0.13.0";
 
   /**
    * The current implementation of the containers.
