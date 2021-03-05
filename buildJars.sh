02655b581c4b8eed67c05379e8ee3c875da7fc9c
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-03-05 15:07:21 +0100

    Add common to the jars to build.

--- buildJars.sh
+++ buildJars.sh
@@ -3,7 +3,7 @@ if [ -f /.dockerenv ]; then
 else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	pushd $DIR >/dev/null
-	declare -a ComponentArray=("profile-manager" "task-manager" "interaction-protocol-engine")
+	declare -a ComponentArray=("common" "profile-manager" "task-manager" "interaction-protocol-engine")
 	for component in "${ComponentArray[@]}"; do
 		pushd $DIR/../$component > /dev/null
 		docker run -v ${HOME}/.m2/repository:/root/.m2/repository -v ${PWD}:/app -it internetofus/$component:dev mvn clean install -DskipTests
