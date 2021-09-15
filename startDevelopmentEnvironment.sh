8d6eaf6ed66ce570a328b19604faa8f5a0dfa434
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-09-15 08:32:44 +0200

    Fix debug port

--- startDevelopmentEnvironment.sh
+++ startDevelopmentEnvironment.sh
@@ -5,7 +5,7 @@ else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	pushd "$DIR" >/dev/null
 	DOCKER_BUILDKIT=1 docker build -f src/dev/docker/Dockerfile -t internetofus/common:dev .
-	docker run --rm --name wenet_common_dev -v /var/run/docker.sock:/var/run/docker.sock -v "${HOME}/.m2":/root/.m2  -v "${PWD}":/app -p 8000:5005 -it internetofus/common:dev /bin/bash
+	docker run --rm --name wenet_common_dev -v /var/run/docker.sock:/var/run/docker.sock -v "${HOME}/.m2":/root/.m2  -v "${PWD}":/app -p 5000:5005 -it internetofus/common:dev /bin/bash
 	./stopDevelopmentEnvironment.sh
 	popd >/dev/null
 fi
