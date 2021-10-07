643b8783347fc7936550ac96af7036c8050aa0c2
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-10-07 19:20:54 +0200

    Fix versions

--- components/src/test/java/eu/internetofus/common/components/Containers.java
+++ components/src/test/java/eu/internetofus/common/components/Containers.java
@@ -71,7 +71,7 @@ public class Containers extends MongoContainer<Containers> implements WeNetCompo
   /**
    * The name of the WeNet task manager docker container to use.
    */
-  public static final String WENET_TASK_MANAGER_DOCKER_NAME = "internetofus/task-manager:0.13.1";
+  public static final String WENET_TASK_MANAGER_DOCKER_NAME = "internetofus/task-manager:0.14.0";
 
   /**
    * The name of the WeNet interaction manager docker container to use.
