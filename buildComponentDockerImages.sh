113bba195fdf667003d1c54faa0235acb82f365e
Bruno Rosell i Gui <rosell@iiia.csic.es>
2022-03-17 16:36:35 +0100

    Return result code

--- buildComponentDockerImages.sh
+++ buildComponentDockerImages.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -f /.dockerenv ]; then
    echo "You can not build docker component images inside a docker container"
+   RESULT=1
 else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	pushd "$DIR" >/dev/null
@@ -9,13 +10,13 @@ else
 		echo "* $component"
 		pushd "$DIR/../$component" > /dev/null
 		./buildDockerImage.sh $@
-		if [ $? -eq 0 ]; then
+		RESULT=$?
+		popd >/dev/null
+		if [ $RESULT -ne 0 ]; then
 			popd >/dev/null
-			popd >/dev/null
-			exit 1
-		else
-			popd >/dev/null
+			break
 		fi
 	done
 	popd >/dev/null
 fi
+exit $RESULT
